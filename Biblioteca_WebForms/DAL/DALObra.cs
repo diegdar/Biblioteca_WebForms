1a8fac83da4be893c9655bfa0f1f12fca491ac8b
Gustavo <gustavomanconi@gmail.com>
2024-12-17 16:17:47 +0100

    Cambio en la B.D. Obra, Genero, Autor

--- Biblioteca_WebForms/DAL/DALObra.cs
+++ Biblioteca_WebForms/DAL/DALObra.cs
@@ -35,35 +35,12 @@ namespace Biblioteca_WebForms.DAL
             }
         }
 
-        public bool Insert(Obra obra, List<int> listaIdGenero, List<int> ListaIdAutor)
+        public bool Insert(Obra obra)
         {
             try
             {
                 dataDB.Obras.InsertOnSubmit(obra);
                 dataDB.SubmitChanges();
-
-                foreach (var generoId in listaIdGenero)
-                {
-                    var obraGenero = new ObraGenero
-                    {
-                        FKObra = obra.IdObra,
-                        FKGenero = generoId
-                    };
-                    dataDB.ObraGeneros.InsertOnSubmit(obraGenero);
-                }
-
-                foreach (var autorId in ListaIdAutor)
-                {
-                    var obraAutor = new AutorObra
-                    {
-                        FKObra = obra.IdObra,
-                        FKAutor = autorId
-                    };
-                    dataDB.AutorObras.InsertOnSubmit(obraAutor);
-                }
-
-                dataDB.SubmitChanges(); // Guardar las dos relaciones, Autor y Genero
-
                 return true;
             }
             catch (Exception ex)
@@ -73,45 +50,15 @@ namespace Biblioteca_WebForms.DAL
             }
         }
 
-        public bool Update(Obra obra, List<int> listaIdGenero, List<int> ListaIdAutor)
+        public bool Update(Obra obra)
         {
             try
             {
                 var obraExistente = dataDB.Obras.Where(ob => ob.IdObra ==obra.IdObra).FirstOrDefault();
                 obraExistente.Titulo = obra.Titulo;
                 obraExistente.Sinopsis = obra.Sinopsis;
-
-                // Primero, eliminar las relaciones existentes
-                // Actualizar las relaciones con los géneros nuevos
-
-                var generosExistentes = dataDB.ObraGeneros.Where(ob => ob.FKObra == obra.IdObra).ToList();
-                dataDB.ObraGeneros.DeleteAllOnSubmit(generosExistentes);
-
-                foreach (var generoId in listaIdGenero)
-                {
-                    var obraGenero = new ObraGenero
-                    {
-                        FKObra = obra.IdObra,
-                        FKGenero = generoId
-                    };
-                    dataDB.ObraGeneros.InsertOnSubmit(obraGenero);
-                }
-
-                // Primero, eliminar las relaciones existentes
-                // Actualizar las relaciones con los autores nuevos
-
-                var autoresExistentes = dataDB.AutorObras.Where(oa => oa.FKObra == obra.IdObra).ToList();
-                dataDB.AutorObras.DeleteAllOnSubmit(autoresExistentes);
-
-                foreach (var autorId in ListaIdAutor)
-                {
-                    var obraAutor = new AutorObra
-                    {
-                        FKObra = obra.IdObra,
-                        FKAutor = autorId
-                    };
-                    dataDB.AutorObras.InsertOnSubmit(obraAutor);
-                }
+                obraExistente.FKAutor = obra.FKAutor;
+                obraExistente.FKGenero = obra.FKGenero;
 
                 dataDB.SubmitChanges();
                 return true;
@@ -128,19 +75,7 @@ namespace Biblioteca_WebForms.DAL
             try
             {
                 var obra = dataDB.Obras.Where(ob=>ob.IdObra == idObra).FirstOrDefault();
-
-                // Eliminar las relaciones con los generos
-                var generosExistentes = dataDB.ObraGeneros.Where(ob => ob.FKObra == idObra).ToList();
-                dataDB.ObraGeneros.DeleteAllOnSubmit(generosExistentes);
-
-                // Eliminar las relaciones con los autores
-                var autoresExistentes = dataDB.AutorObras.Where(oa => oa.FKObra == idObra).ToList();
-                dataDB.AutorObras.DeleteAllOnSubmit(autoresExistentes);
-
-                // Eliminar la obra
                 dataDB.Obras.DeleteOnSubmit(obra);
-
-                // Guarda los cambios
                 dataDB.SubmitChanges();
                 return true;
             }
