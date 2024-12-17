6b04dc2aeb615170dd3fc31ec3e32ea99e3b4e22
diegdar <diegdarch@hotmail.com>
2024-12-17 12:07:28 +0100

    feat: creacion clase CommontMethod

--- Biblioteca_WebForms/Pagina/Login.aspx.cs
+++ Biblioteca_WebForms/Pagina/Login.aspx.cs
@@ -9,6 +9,13 @@ namespace Biblioteca_WebForms.Pagina
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private CommonMethods comMethods;
+
+        public WebForm1()
+        {
+            comMethods = new CommonMethods();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
--- Biblioteca_WebForms/Pagina/index.aspx.cs
+++ Biblioteca_WebForms/Pagina/index.aspx.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using Biblioteca_WebForms;
+using Biblioteca_WebForms.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Biblioteca.Pagina
 {
+
+
     public partial class index : System.Web.UI.Page
     {
+        private CommonMethods comMethods;
+
+        public index()
+        {
+            comMethods = new CommonMethods();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            DALEjemplar DALEjemplar = new DALEjemplar();
+            DALEjemplar.Delete(5);
 
         }
+
+
     }
 }
